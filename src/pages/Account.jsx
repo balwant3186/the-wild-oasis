8198385ea6e9ca6af52c19b7ad33facd876b71ad
Balwant <balwantsingh3186@gmail.com>
2024-06-04 20:32:08 +0530

    update form commented

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,5 +1,5 @@
 import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
-import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
+// import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
@@ -13,10 +13,10 @@ function Account() {
         <UpdateUserDataForm />
       </Row>
 
-      <Row>
+      {/* <Row>
         <Heading as="h3">Update password</Heading>
         <UpdatePasswordForm />
-      </Row>
+      </Row> */}
     </>
   );
 }
